843550688a34d4a737977232cddd984cc8dd1cf9
Den <zgunnar@yandex.ru>
2021-11-26 17:20:30 +0300

    lesson1 react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+//<img src={logo} className="App-logo" alt="logo" />
+
+/*
+инлайновый стиль <div style={{paddingTop: '25px', border: '1px solid white'}}>
+<header className="App-header"> --->>>  import './App.css';
+
+*/
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="App">
+        <header
+      className={`App-header ${props.showRed ? 'header-red' : 'header-blue'}`}                         
+      style={{top: props.topPosition || '10px'}}
+     >
+
+        My First React App
+        <h3>Hello, {props.name}</h3>
+        <div>
+        <h3>Домашнее задание</h3>
+        <p style={{fontSize: '18px', width:'400px'}}>1.Развернуть новый проект с использованием create-react-app. - + </p>
+        <p style={{fontSize: '18px', width:'700px'}}>Проект создал, как описано в методичке и на уроке </p>
+        <p style={{fontSize: '18px', width:'400px'}}>3.Стилизовать компоненты через css (при желании можно использовать less или sass, однако для sass нужно дополнительно установить node-sass: документация CRA). </p>
+        <p style={{fontSize: '18px', width:'800px'}}>разобрал что такое инлайновый стиль и className=</p>
+        <p style={{fontSize: '18px', width:'400px'}}>5.Установить расширение React Devtools. Установил в фаерфоксе так как хромом не пользуюсь, похоже на Vue Devtools </p>
+
+
+
+
+
+
+          
+           </div>
+        <div style={{paddingTop: '25px', border: '1px solid white'}}>
+    Inline Styles
+   </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
